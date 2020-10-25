4bd0d51dba7b13d96771399299585088dfca6cee
Brian Balsamo <Brian@BrianBalsamo.com>
2020-10-24 20:48:20 -0500

    Bump version: 0.2.0 → 0.3.0

--- src/ledge/__init__.py
+++ src/ledge/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = "Brian Balsamo"
 __email__ = "Brian@BrianBalsamo.com"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 from ._bases import HandlerImplementation, ResponderImplementation
